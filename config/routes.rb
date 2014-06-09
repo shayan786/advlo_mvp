3efa0d936d42faf7cd23ce321fb5c49ed52a02df
Shayan <shayandhanani@gmail.com>
2014-06-09 15:47:47 -0600

    small css changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:omniauth_callbacks => "omniauth_callbacks"}
   root 'application#homepage'
   
+  #resources :adventures
 
-end
+end	
