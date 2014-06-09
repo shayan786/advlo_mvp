7789bf116ecf387638edf7dd29957eabdfa589e1
SerKnight <chrisknight.mail@gmail.com>
2014-06-09 15:17:59 -0600

    user / spec / active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users, :controllers => {:omniauth_callbacks => "omniauth_callbacks"}
   root 'application#homepage'
   
---
+++ spec/features/facebook_oauth_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+
+describe "Creating user from facebook oauth" do 
+  xit 'saves the user profile image url' do 
+
+  end
+end
