92006666460a54c5e3f19cf3f6cedd432852cfdc
SerKnight <chrisknight.mail@gmail.com>
2014-06-10 12:04:49 -0600

    production mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,15 +16,14 @@ class User < ActiveRecord::Base
       if registered_user
         return registered_user
       else
-        
         user = User.create( 
-                            name: auth.extra.raw_info.name,
-                            avatar_url: auth.info.image,
-                            provider: auth.provider,
-                            uid: auth.uid,
-                            email: auth.info.email,
-                            password: Devise.friendly_token[0,20]
-                          )
+          name: auth.extra.raw_info.name,
+          avatar_url: auth.info.image,
+          provider: auth.provider, 
+          uid: auth.uid, 
+          email: auth.info.email, 
+          password: Devise.friendly_token[0,20] 
+        )
       end    
     end
   end
