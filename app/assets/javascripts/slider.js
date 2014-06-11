a031632acde304293aa163a7478dd2abb1b88fe9
SerKnight <chrisknight.mail@gmail.com>
2014-06-11 16:28:18 -0600

    font is awesome

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -3,7 +3,14 @@ $(function(){
     //Your options here:
     mode:'horizontal',
     loop: true,
-    slidesPerView: 3
+    slidesPerView: 3,
+    keyboardControl: true,
     //etc..
   });
+  $('#next-slide').click(function(){
+    mySwiper.swipeNext()
+  })
+  $('#prev-slide').click(function(){
+    mySwiper.swipePrev()
+  })
 })
