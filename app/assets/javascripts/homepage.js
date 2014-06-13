f52bc4ada9bee0de5f6c160d242bded5a75ea8a5
SerKnight <chrisknight.mail@gmail.com>
2014-06-13 12:17:33 -0600

    hover states and location. might want to use proper image size for orange png and not resize large image

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,19 +1,15 @@
 $(document).ready(function(){
   $('.hover-block').hover(function(){
-    
     $('.copy-'+$(this).attr('id')).stop().animate({
       opacity: 0
     },500)
-
     $(this).stop().animate({
       opacity: 1
     })
   },function(){
-
     $('.copy-'+$(this).attr('id')).stop().animate({
       opacity: 1
     },300)
-
     $(this).stop().animate({
       opacity: 0
     })
