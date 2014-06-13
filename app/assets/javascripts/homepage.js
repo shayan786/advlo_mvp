38ba59001277dadb6c14064820e94bc3633fc0ad
SerKnight <chrisknight.mail@gmail.com>
2014-06-13 13:08:25 -0600

    hover over title doesnt show guid info

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,17 +1,23 @@
 $(document).ready(function(){
-  $('.hover-block').hover(function(){
-    $('.copy-'+$(this).attr('id')).stop().animate({
-      opacity: 0
-    },500)
-    $(this).stop().animate({
-      opacity: 1
+  function adventureHover(){
+    $('.hover-block').hover(function(){
+
+      $('.copy-'+$(this).attr('id')).stop().animate({
+        opacity: 0
+      },500)
+
+      $(this).stop().animate({
+        opacity: 1
+      })
+    },function(){
+      $('.copy-'+$(this).attr('id')).stop().animate({
+        opacity: 1
+      },300)
+      $(this).stop().animate({
+        opacity: 0
+      })
     })
-  },function(){
-    $('.copy-'+$(this).attr('id')).stop().animate({
-      opacity: 1
-    },300)
-    $(this).stop().animate({
-      opacity: 0
-    })
-  })
+  }
+
+  adventureHover();
 })
