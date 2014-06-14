433898a30ff7be8ed453e162154bc634571a8f93
Shayan <shayandhanani@gmail.com>
2014-06-13 21:40:42 -0500

    host conditions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
-  
   def homepage
     @feat_adventures = Adventure.all
   end
+
+  def host
+  end
+
 end
