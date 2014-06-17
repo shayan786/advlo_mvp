4df3ceead95b49602f93ac72b560aa1ef60279df
SerKnight <chrisknight.mail@gmail.com>
2014-06-17 15:30:03 -0600

    javascript catch for show page

--- app/assets/javascripts/adventure.js
+++ app/assets/javascripts/adventure.js
@@ -56,12 +56,74 @@ function sizeSidebar(){
   $('.adventure-show-infographic').width($('.navigation-brick').last().width() - 28)
 }
 
+
+function navBarCatch(){
+  
+  $('#main_image img').load(function(){
+    $(window).scroll(function(){
+      var s = $(window).scrollTop()
+      if(s > widthOfBrowserHeight()){
+        fixSidebar()        
+      }else{
+        resetSideBar()        
+      }
+    })
+  })
+}
+
+function widthOfBrowserHeight(){
+  var windowWidth = $(window).width()
+  if( windowWidth > 1090 ){
+    return 290;
+  }else if( windowWidth > 1060){
+    return 280;
+  }else if( windowWidth > 1040){
+    return 270;
+  }else if( windowWidth > 1020){
+    return 263;
+  }else if( windowWidth > 1000){
+    return 240;
+  }else if( windowWidth > 900){
+    return 225;
+  }
+}
+
+
+
+function fixSidebar(){
+  $('#desktop-adventure-nav').css({
+    position: 'fixed',
+    right: '0px',
+    top: '481px'
+  })
+}
+
+function resetSideBar(){
+  $('#desktop-adventure-nav').css({
+    position: 'relative',
+    top: '0px'
+  })
+}
+
+
+  // .adventure-show-container .adventure-copy-wrapper .navigation-brick {
+  // position: fixed;
+  // float: left;
+  // margin-top: 20px;
+  // height: 100%;
+  // height: 200px;
+  // border: 1px solid #C35B26;
+  // top: 480px;
+  // right: 15px;
+  // width: 23%;
+
 adventureInit = function() {
   sizeSidebar()
   adventureHover();
   heroScroll();
   filterCatch();
   masonrySetup();
+  navBarCatch(); 
 };
 
 $(window).resize(function(){
@@ -70,3 +132,7 @@ $(window).resize(function(){
 
 $(document).ready(adventureInit);
 $(document).on('page:load', adventureInit);
+
+
+
+
