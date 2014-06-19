f7f352f6e8a17e5e56c7c7fe77a88525376211ee
SerKnight <chrisknight.mail@gmail.com>
2014-06-18 18:02:21 -0600

    gotta be a before save

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -8,7 +8,7 @@ class Adventure < ActiveRecord::Base
 
   validates_uniqueness_of :title
 
-  after_save :set_slug
+  before_save :set_slug
 
   #Generate display url accessable to viewers / hosts e.g. adventures/go-to-this-awesome-place
   def set_slug
