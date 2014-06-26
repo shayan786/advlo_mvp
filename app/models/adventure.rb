d4dc7706db2e755615575d86a9d927ebe3f56ec0
SerKnight <chrisknight.mail@gmail.com>
2014-06-26 15:32:14 -0600

    relation for itineraries and active admin

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -2,10 +2,11 @@ class Adventure < ActiveRecord::Base
   before_save :set_slug
 
   has_many :user_adventures
+  has_many :itineraries
   has_many :users, through: :user_adventures
   has_many :adventure_gallery_images
 
-  accepts_nested_attributes_for :user_adventures, :adventure_gallery_images, :allow_destroy => true
+  accepts_nested_attributes_for :user_adventures, :adventure_gallery_images, :itineraries, :allow_destroy => true
 
   has_attached_file :attachment, :styles => { :large => "600x750>", :medium => "325x285>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
