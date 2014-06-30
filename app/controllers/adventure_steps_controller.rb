421bf8235f311d6cd8b6bab0315d481f8c535231
Shayan <shayandhanani@gmail.com>
2014-06-30 10:57:31 -0600

    adv iten additions

--- app/controllers/adventure_steps_controller.rb
+++ app/controllers/adventure_steps_controller.rb
@@ -42,17 +42,40 @@ class AdventureStepsController < ApplicationController
       @adv_img_to_delete.destroy
 
       respond_to do |format|
-        format.js
+        format.js {}
       end
     # Hook for adding a itenerary item and remain on the same page
     elsif params[:add_iten_item] == "1"
-      @adventure.itineraries.create(params[:itinerary])
+      @adventure.itineraries.create(headline: params[:headline], description: params[:description], adventure_id: @adventure.id)
 
       respond_to do |format|
-        format.html {redirect_to "/adventure_steps/itinerary?adventure_id=#{@adventure.id}", notice: "Itenerary has been updated!"}
+        format.html {redirect_to "/adventure_steps/itinerary?adventure_id=#{@adventure.id}", notice: "Itenerary event '#{params[:headline]}' has been created!"}
         format.js {}
       end
 
+    # Hook for updating/deleting itenerary item and remain on the same page
+    elsif params[:update_iten_item] == "1"
+      iten_id = params[:iten_id].to_i
+      @adv_iten_item = @adventure.itineraries.find(iten_id)
+      
+      # Delete the iten item
+      if params[:delete] == "1"
+        @adv_iten_item.destroy
+
+        respond_to do |format|
+          format.html {redirect_to "/adventure_steps/itinerary?adventure_id=#{@adventure.id}", notice: "Itenerary event '#{@adv_iten_item.headline}' has been deleted!"}
+          format.js {}
+        end
+      end
+      # Update the iten item
+      if params[:update] == "1"
+        @adv_iten_item.update(headline: params[:headline], description: params[:description])
+
+        respond_to do |format|
+          format.html {redirect_to "/adventure_steps/itinerary?adventure_id=#{@adventure.id}", notice: "Itenerary event '#{@adv_iten_item.headline}' has been updated!"}
+          format.js {}
+        end
+      end
     else
       @adventure.attributes = adventure_params
       render_wizard @adventure
@@ -65,6 +88,6 @@ class AdventureStepsController < ApplicationController
   # since you'll be able to reuse the same permit list between create and update. Also, you
   # can specialize this method with per-user checking of permissible attributes.
   def adventure_params
-    params.required(:adventure).permit(:title, :subtitle, :attachment, :location, :summary, :cap_min, :cap_max, :price, :price_type, :duration_num, :duration_type, :category, :other_notes, :adventure_gallery_image, :images, :itineraries, :itinerary)
+    params.required(:adventure).permit(:title, :subtitle, :attachment, :location, :summary, :cap_min, :cap_max, :price, :price_type, :duration_num, :duration_type, :category, :other_notes, :adventure_gallery_image, :images, :itineraries, :itinerary, :headline, :description)
   end
 end
