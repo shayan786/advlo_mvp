9033479f354e93206a8383af997464ee1c09a6c7
SerKnight <chrisknight.mail@gmail.com>
2014-06-30 22:10:09 -0600

    added capybara selenium - adventure spec

--- db/schema.rb
+++ db/schema.rb
@@ -81,6 +81,8 @@ ActiveRecord::Schema.define(version: 20140630224718) do
     t.text     "other_notes"
   end
 
+  add_index "adventures", ["slug"], name: "index_adventures_on_slug", using: :btree
+
   create_table "events", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -88,6 +90,19 @@ ActiveRecord::Schema.define(version: 20140630224718) do
     t.datetime "start_time"
     t.datetime "end_time"
   end
+
+  create_table "friendly_id_slugs", force: true do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
   create_table "hero_images", force: true do |t|
     t.string   "location"
