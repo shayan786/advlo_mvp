c6934cc7018d17b1d03765eca8da48bc206ede0a
Shayan <shayandhanani@gmail.com>
2014-07-01 18:32:06 -0600

    adv event delete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,9 +9,6 @@ class EventsController < ApplicationController
 		@events_n = @events.where("start_time >= ? and end_time <= ?", start_q, end_q)
 		@events_j = @events_n.all.map{|e| {"id" => "#{e.id}", "title"=>"IB", "allDay" => false, "start"=> e.start_time.to_time.iso8601, "end"=> e.end_time.to_time.iso8601}}
 
-		puts @events_j
-
-
 		respond_to do |format|
 			format.json {render json: @events_j}
 		end  
@@ -25,7 +22,7 @@ class EventsController < ApplicationController
 		if @event.save
 			respond_to do |format|
 				format.js {render "create.js", layout: false}
-				format.json {render json: {success: "true"}}
+				format.json {render json: {"title"=>"IB", "allDay" => false, "start"=> @event.start_time.to_time.iso8601, "end"=> @event.end_time.to_time.iso8601}}
 			end
 		end
 	end
@@ -35,7 +32,13 @@ class EventsController < ApplicationController
 	end
 
 	def destroy
+		@event = Event.find(params[:id])
 
+		@event.destroy
+
+		respond_to do |format|
+			format.json {render json: {deleted: "true"}}
+		end
 	end
 
 	private
