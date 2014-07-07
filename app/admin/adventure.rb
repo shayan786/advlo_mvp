eabc8c1ccff14e03b70b49b75d6102720e0a4de8
SerKnight <chrisknight.mail@gmail.com>
2014-07-07 11:27:31 -0600

    csv uploader - resetting heroku

--- app/admin/adventure.rb
+++ app/admin/adventure.rb
@@ -1,4 +1,17 @@
 ActiveAdmin.register Adventure do
+  action_item :only => :index do
+    link_to 'Upload CSV', :action => 'upload_csv'
+  end
+
+  collection_action :upload_csv do
+    render "admin/csv/upload_csv"
+  end
+
+  collection_action :import_csv, :method => :post do
+    CsvDb.convert_save("adventure", params[:dump][:file])
+    redirect_to :action => :index, :notice => "CSV imported successfully!"
+  end
+
 
   before_filter :only => [:show, :edit, :update, :destroy] do
     @adventure = Adventure.find_by_title(params[:id])
--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,21 @@
 ActiveAdmin.register User do
 
+  
+  action_item :only => :index do
+    link_to 'Upload CSV', :action => 'upload_csv'
+  end
+
+  collection_action :upload_csv do
+    render "admin/csv/upload_csv"
+  end
+
+  collection_action :import_csv, :method => :post do
+    CsvDb.convert_save("adventure", params[:dump][:file])
+    redirect_to :action => :index, :notice => "CSV imported successfully!"
+  end
+
+
+
   index do
     column :email
     column :name
