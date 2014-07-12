5a321306ab16128d9b0146925fc60e5dcb1ac2ad
SerKnight <chrisknight.mail@gmail.com>
2014-07-12 14:21:09 -0600

    grid on homepage and going to refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def homepage
-    @feat_adventures = Adventure.order('created_at DESC').limit(5)
+    @feat_adventures = Adventure.order('created_at DESC').limit(6)
     @feat_hosts = User.where(is_guide: true).limit(6)
   end
 
