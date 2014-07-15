000e56f49e0439197065c62bdf42a765c56e7e57
SerKnight <chrisknight.mail@gmail.com>
2014-07-15 16:58:21 -0600

    similar adv 2 similar 2 tags

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -43,11 +43,10 @@ class AdventuresController < ApplicationController
     @all_adventure_events = @adventure.events.where("capacity > 0").sort_by{|a| a.start_time}
     @limited_adventure_events = @adventure.events.where("capacity > 0").sort_by{|a| a.start_time}.take(5)
 
-    # related = []
-    # related << Adventure.where('category LIKE ?',"%#{@adventure.category}%").limit(2) 
-    # related << @adventure.nearbys(20).limit(2) if @adventure.nearbys(20)
-    # @related = related.flatten
-    @related = Adventure.all
+    related = []
+    related << Adventure.where('category LIKE ?',"%#{@adventure.category}%").limit(2) 
+    related << @adventure.nearbys(20).limit(2) if @adventure.nearbys(20)
+    @related = related.flatten
     @reservation = Reservation.new
   end
 
