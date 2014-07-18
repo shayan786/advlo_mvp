daed413d6e53f014f34a282478735dbc4cf10ffe
SerKnight <chrisknight.mail@gmail.com>
2014-07-18 14:59:28 -0600

    background jobs table for active admin - and refactored adn organized bookings and payouts

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -10,24 +10,22 @@ ActiveAdmin.register_page "Dashboard" do
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+    section "Background Jobs" do
+      now = Time.now.getgm
+      ul do
+        li do
+          jobs = Delayed::Job.where('failed_at is not null').count(:id)
+          link_to "#{jobs} failing jobs", admin_jobs_path(q: {failed_at_is_not_null: true}), style: 'color: red'
+        end
+        li do
+          jobs = Delayed::Job.where('run_at <= ?', now).count(:id)
+          link_to "#{jobs} late jobs", admin_jobs_path(q: {run_at_lte: now.to_s(:db)}), style: 'color: hsl(40, 100%, 40%)'
+        end
+        li do
+          jobs = Delayed::Job.where('run_at >= ?', now).count(:id)
+          link_to "#{jobs} scheduled jobs", admin_jobs_path(q: {run_at_gte: now.to_s(:db)}), style: 'color: green'
+        end
+      end
+    end
+  end
 end
