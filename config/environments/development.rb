09d10a15cd8ff8e1129a7708f35645ae0b72d7a6
SerKnight <chrisknight.mail@gmail.com>
2014-07-21 15:44:49 -0600

    mailer and css

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "127.0.0.1", :port => 1025 }
 
-
   config.serve_static_assets = true
 end
