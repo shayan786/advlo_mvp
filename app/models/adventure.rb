f76fa8cac0ea98f0428e53404ee3c0f76f5b7bcb
SerKnight <chrisknight.mail@gmail.com>
2014-07-22 17:29:30 -0600

    process in background

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -11,7 +11,6 @@ class Adventure < ActiveRecord::Base
 
   has_attached_file :attachment, :styles => { :hero => "1280X720>", :large => "600x750>", :medium => "325x285>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
-
   process_in_background :attachment
 
   validates_uniqueness_of :title
--- app/models/hero_image.rb
+++ app/models/hero_image.rb
@@ -1,5 +1,6 @@
 class HeroImage < ActiveRecord::Base
   has_attached_file :attachment, :styles => { hero: '1024x720', thumb: '100x100'}, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
+  process_in_background :attachment
 
 end
