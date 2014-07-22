2a416b21513945a090c5ed80352389ccc04728db
Shayan <shayandhanani@gmail.com>
2014-07-22 13:01:14 -0600

    adv modal hide show refresh page logic

--- app/views/reservations/create.js
+++ app/views/reservations/create.js
@@ -1,11 +1,15 @@
 $('#new_reservation').remove();
 $('#stripe-error-message').remove();
-// $('.modal-backdrop').first().remove();
-// $('#event-modal').toggle();
 
 $('#success').show();
 $('#success-detail').show();
-$('#advlo-logo').show()
+$('#advlo-logo').show();
 
 $('#success').text('You are going on an adventure!');
 $('#success-detail').text("You will receive an email confirmation shortly with all the adventure details. You can now contact the guide to setup any specific details. Can't wait to hear about your experience");
+
+
+// If user clicks out of the thankyou modal, refresh the page
+$('#reservations-modal').on('hide.bs.modal', function(e){
+	window.location.reload(true);
+})
--- app/views/reservations/request.js
+++ app/views/reservations/request.js
@@ -1,7 +1,5 @@
 $('form#request_reservation').remove();
 $('#request_reservation #stripe-error-message').remove();
-// $('.modal-backdrop').first().remove();
-// $('#event-modal').toggle();
 
 $('#request_reservation #success').show();
 $('#request_reservation #success-detail').show();
@@ -9,3 +7,8 @@ $('#request_reservation #advlo-logo').show()
 
 $('#request_reservation #success').text('Your reservation request has been received!');
 $('#request_reservation #success-detail').text("You will be notified via email when the host accepts or rejects your reservation request");
+
+// If user clicks out of the thankyou modal refresh the page
+$('#reservations-request-modal').on('hide.bs.modal', function(e){
+	window.location.reload(true);
+})
