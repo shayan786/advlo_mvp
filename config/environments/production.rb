d1b36983984fec4e81241a2cd945f414ce98d51a
SerKnight <chrisknight.mail@gmail.com>
2014-07-23 11:07:44 -0600

    serving wrong assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'http://adventure-local.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'beta.advlo.com' }
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
