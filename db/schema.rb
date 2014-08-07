ff3362ad978c73a2bc4e652d649f9cb1af2e2235
SerKnight <chrisknight.mail@gmail.com>
2014-08-07 00:09:58 -0600

    added video_url to adventures

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140804220017) do
+ActiveRecord::Schema.define(version: 20140807053503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 20140804220017) do
     t.string   "region"
     t.string   "rating",                  default: ""
     t.string   "country"
+    t.string   "video_url"
   end
 
   create_table "contact_advlos", force: true do |t|
