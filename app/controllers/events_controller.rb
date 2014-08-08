8053f2804f07c898b124648ca3425e075e67d1dd
SerKnight <chrisknight.mail@gmail.com>
2014-08-08 01:35:45 -0600

    time zone param fullcal

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,7 +11,7 @@ class EventsController < ApplicationController
 		end_q = Time.at(params[:end].to_i)
 
 		@events_n = @events_open.where("start_time >= ? and end_time <= ?", start_q, end_q)
-		@events_j = @events_n.all.map{|e| {"id" => "#{e.id}", "title"=> @adventure.title, "allDay" => false, "start"=> e.start_time.to_time.iso8601, "end"=> e.end_time.to_time.iso8601}}
+		@events_j = @events_n.all.map{|e| {"id" => "#{e.id}", "title"=> @adventure.title, "allDay" => false, "timezoneParam" => "UTC", "start"=> e.start_time.to_time.iso8601, "end"=> e.end_time.to_time.iso8601}}
 
 		respond_to do |format|
 			format.json {render json: @events_j}
@@ -29,7 +29,7 @@ class EventsController < ApplicationController
 		end_q = Time.at(params[:end].to_i)
 
 		@events_n = @events_reserved.where("start_time >= ? and end_time <= ?", start_q, end_q)
-		@events_j = @events_n.all.map{|e| {"id" => "#{e.id}", "title"=> @adventure.title, "allDay" => false, "start"=> e.start_time.to_time.iso8601, "end"=> e.end_time.to_time.iso8601}}
+		@events_j = @events_n.all.map{|e| {"id" => "#{e.id}", "title"=> @adventure.title, "allDay" => false, "timezoneParam" => "UTC", "start"=> e.start_time.to_time.iso8601, "end"=> e.end_time.to_time.iso8601}}
 
 		respond_to do |format|
 			format.json {render json: @events_j}
