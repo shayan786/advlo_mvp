53eb44f3dfe0cfaac70f97453592418e7dfd3036
Shayan <shayandhanani@gmail.com>
2014-08-14 13:34:50 -0600

    changes to webhook

--- app/controllers/stripe_hooks_controller.rb
+++ app/controllers/stripe_hooks_controller.rb
@@ -6,15 +6,31 @@ class StripeHooksController < ApplicationController
 
     receiving_data = JSON.parse request.body.read
 
-    puts receiving_data['data']['object']
+    puts receiving_data['data']['object']['status']
     puts receiving_data.inspect
 
+    update_payout(receiving_data)
 
     respond_to do |format|
       format.json {render json: {status: 200}}
     end 
   end
+
+
+  private
+
+  def update_payout(receiving_data)
+    puts "****"
+    puts receiving_data.object.id
+
+    #@payout = Payout.find_by_stripe_transfer_id(receiving_data.object.id)
+
+
+
+  end
   
 
 end
+
+
 
