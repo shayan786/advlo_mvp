dbe3c2ac16bcf9f45c790652b612dd3e2cf8ad78
SerKnight <chrisknight.mail@gmail.com>
2014-08-25 19:21:52 -0600

    config and ipad

--- config/initializers/switch_user.rb
+++ config/initializers/switch_user.rb
@@ -24,13 +24,13 @@ SwitchUser.setup do |config|
   # if it returns true, the request will continue, 
   # else the request will be refused and returns "Permission Denied"
   # if you switch from "admin" to user, the current_user param is "admin"
-  config.controller_guard  = lambda { |current_user, request| Rails.env.development? || ( Rails.env.production? && current_user && (current_user.email == 'chrisknight.mail@gmail.com' || current_user.email == 'shayan@advlo.com' || current_user.email == 'jon@advlo.com')) }
+  config.controller_guard  = lambda { |current_user, request| Rails.env.development? || ( Rails.env.production? && current_user && (current_user.email == 'chrisknight.mail@gmail.com' || current_user.email == 'shayan@advlo.com' || current_user.email == 'jemaser@syr.edu')) }
 
   # view_guard is a block, 
   # if it returns true, the switch user select box will be shown, 
   # else the select box will not be shown
   # if you switch from admin to "user", the current_user param is "user"
-  config.view_guard =  lambda { |current_user, request| Rails.env.development? || ( Rails.env.production? && current_user && (current_user.email == 'chrisknight.mail@gmail.com' || current_user.email == 'shayan@advlo.com' || current_user.email == 'jon@advlo.com')) }
+  config.view_guard =  lambda { |current_user, request| Rails.env.development? || ( Rails.env.production? && current_user && (current_user.email == 'chrisknight.mail@gmail.com' || current_user.email == 'shayan@advlo.com' || current_user.email == 'jemaser@syr.edu')) }
 
   # redirect_path is a block, it returns which page will be redirected 
   # after switching a user.
