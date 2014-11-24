d6bf77c7f7e5359aed6c8cbe79c2c2882e77f3ce
Shayan <shayandhanani@gmail.com>
2014-11-24 13:16:55 -1000

    investor page edits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,10 @@ class ApplicationController < ActionController::Base
   end
 
   def invest
-
+    @adventures = [] 
+    @adventures << Adventure.find_by_slug('speedflying-basic-pilot')
+    @adventures << Adventure.find_by_slug('essential-mountain-bike-skills')
+    @adventures << Adventure.find_by_slug('the-pearl-islands-adventure')
   end
 
   def store_location
