78ae53be4341128869b7a7ed6acebaa96d10299f
Shayan <shayandhanani@gmail.com>
2015-01-08 14:21:30 -1000

    blah

--- app/models/user.rb
+++ app/models/user.rb
@@ -142,6 +142,7 @@ class User < ActiveRecord::Base
   def get_user_geocode_info
     user_geocode_info = Hash.new
 
+    Geocoder.configure(:timeout => 10000)
     geocode_obj = Geocoder.search(current_sign_in_ip)
 
     user_geocode_info["lat"] = geocode_obj[0].data['latitude']
