f0ab5d8631aa92444b8fc03e88f6888273cd88c6
SerKnight <chrisknight.mail@gmail.com>
2015-01-13 11:27:51 -1000

    succes for fb ui

--- app/assets/javascripts/promotion.js
+++ app/assets/javascripts/promotion.js
@@ -40,9 +40,9 @@ function initial_share_fb_init(current_user_id){
             url: "giveaway/"+current_user_id
           })
         } else {
-          $.ajax({
-            method: 'POST',
-            url: "giveaway/"+current_user_id
+          swal({     
+            title: "Enter giveaway later?",
+            imageUrl: "http://i.imgur.com/a6L0hYB.png"
           })
         }
       }
@@ -84,7 +84,6 @@ function adv_share_social() {
     window.open("https://twitter.com/share?url=" + url + "&text=%23AdventureLocal - Adventure local is giving away $1000 towards any trip. " + url, 'sharer', 'top=' + (height/3) + ',left=' + (width/3) + ',toolbar=0,status=0,width=' + 500 + ',height=' + 500);
   })
 }
-
 
 function masonry_init(){
   $('#promotion-wrapper .additional_entries .share_adventures').masonry({
