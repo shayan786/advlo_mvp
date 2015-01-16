ab67e38109b0f9845fdbadee481683891a71e946
Shayan <shayandhanani@gmail.com>
2015-01-15 19:06:29 -1000

    nearby adventures

--- app/mailers/advlo_mailer.rb
+++ app/mailers/advlo_mailer.rb
@@ -319,14 +319,49 @@ class AdvloMailer < ActionMailer::Base
     end
   end
 
-  def market_nearby_adventures(user, nearby_adventures)
+  def market_nearby_adventures(user)
     @user = user
-    @nearby_adventures = nearby_adventures
 
-    mail(to: @user.email, subject: "[ADVLO] : Adventures Near You") do |format|
-      format.html { render layout: 'marketing_advlo_mail' }
-      format.text
+    if user.email_list == true
+      if !user.is_guide
+        # Get location through IP
+        geocode_obj = Geocoder.search(user.current_sign_in_ip)
+
+        lat = geocode_obj[0].data['latitude']
+        long = geocode_obj[0].data['longitude']
+
+        # Other option, if that traveler has specific the categories 'liked' find those then randomly select 3
+        if user.category && user.category != ''
+          user_pref_categories = user.category.split(',')
+
+          category_sql_string = ''
+          user_pref_categories.each_with_index do |cat,i|
+            if (i==0)
+              category_sql_string = "category LIKE '%#{cat}%'"
+            elsif (i > 0)
+              category_sql_string = category_sql_string + " OR category LIKE '%#{cat}%'"
+            end
+          end
+
+          nearby_adventures = Adventure.near([lat,long],150).approved.limit(10).order('RANDOM()').where(category_sql_string).limit(3)
+
+        else
+          # Find nearby adventures 150 miles randomly
+          nearby_adventures = Adventure.near([lat,long],150).approved.limit(10).order('RANDOM()').limit(3)
+        end
+
+        @nearby_adventures = nearby_adventures
+
+        # If there any, then send the email
+        if nearby_adventures.length > 0
+          mail(to: @user.email, subject: "[ADVLO] : Adventures Near You") do |format|
+            format.html { render layout: 'marketing_advlo_mail' }
+            format.text
+          end
+        end
+      end
     end
+    
   end
 
   #--------------------- OUTREACH EMAILS -----------------------------------------
